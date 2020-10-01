c7e3b79f086bcfbed8f351b5466cfbc67254c2a6
Arturo Gómez <artuedu@gmail.com>
2020-10-01 18:53:26 -0500

    v1 CalendarApp

---
+++ public/precache-manifest.e0c815113713a2584e480033c78c1727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcd1b72b4b32065de87b4b2b7a0cb9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f4980209a51650adfb",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "0ba5811931404c898931",
+    "url": "/static/css/main.66d59588.chunk.css"
+  },
+  {
+    "revision": "50f4980209a51650adfb",
+    "url": "/static/js/2.d8eee279.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.d8eee279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba5811931404c898931",
+    "url": "/static/js/main.56e166f4.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
